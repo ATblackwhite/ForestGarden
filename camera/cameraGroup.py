b57b40f50f87036108556750027e2911d9d5eee3
Reno <49602356+yinwx06@users.noreply.github.com>
2023-07-22 17:53:22 +0800

    Add files via upload

--- camera/cameraGroup.py
+++ camera/cameraGroup.py
@@ -5,10 +5,15 @@ class CameraGroup(pygame.sprite.Group):
     def __init__(self):
         super().__init__()
         self.display_surface = pygame.display.get_surface()
+        self.offset = pygame.math.Vector2()
 
-    # 按照层数顺序绘制精灵
-    def custom_draw(self):
+    def custom_draw(self, player):
+        self.offset.x = player.rect.centerx - SCREEN_WIDTH / 2
+        self.offset.y = player.rect.centery - SCREEN_HEIGHT / 2
+
         for layer in LAYERS.values():
             for sprite in self.sprites():
                 if sprite.z == layer:
-                    self.display_surface.blit(sprite.image, sprite.rect)
+                    offset_rect = sprite.rect.copy()
+                    offset_rect.center -= self.offset
+                    self.display_surface.blit(sprite.image, offset_rect)
