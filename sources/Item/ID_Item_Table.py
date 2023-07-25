1cba5722989feaa56ba03d337b733455d3fd6028
Reno <49602356+yinwx06@users.noreply.github.com>
2023-07-25 11:06:13 +0800

    Update ID_Item_Table.py

--- sources/Item/ID_Item_Table.py
+++ sources/Item/ID_Item_Table.py
@@ -10,5 +10,5 @@ ID_Item_Table = {
     "Seed_02.img": 'sources/Item/Seed_02/crops. (2)_121.png',
     'Seed_02.des': 'sources/Item/Seed_02/Description.txt',
     "Seed_03.img": 'sources/Item/Seed_03/crops. (2)_193.png',
-    'Seed_03.des': 'sources/Item/Seed_03/Description.txt',
+    'Seed_03.des': 'sources/Item/Seed_03/Description.txt'
 }
