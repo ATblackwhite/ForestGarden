1a371a6e76473914c72778d44bbe4e96cdf93ec7
Reno <49602356+yinwx06@users.noreply.github.com>
2023-07-25 11:05:09 +0800

    Add files via upload

--- sources/Item/ID_Item_Table.py
+++ sources/Item/ID_Item_Table.py
@@ -4,5 +4,11 @@ ID_Item_Table = {
     "Hoe.des": 'sources/Item/Hoe/Description.txt',
     "Hoe.img": 'sources/Item/Hoe/tools..png',
     "Axe.img": 'sources/Item/Axe/Axe.png',
-    "Axe.des": 'sources/Item/Axe/Description.txt'
+    "Axe.des": 'sources/Item/Axe/Description.txt',
+    "Seed_01.img": 'sources/Item/Seed_01/crops. (2)_169.png',
+    'Seed_01.des': 'sources/Item/Seed_01/Description.txt',
+    "Seed_02.img": 'sources/Item/Seed_02/crops. (2)_121.png',
+    'Seed_02.des': 'sources/Item/Seed_02/Description.txt',
+    "Seed_03.img": 'sources/Item/Seed_03/crops. (2)_193.png',
+    'Seed_03.des': 'sources/Item/Seed_03/Description.txt',
 }
